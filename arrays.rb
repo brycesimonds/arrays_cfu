4800c4718bd92601917eb3bab7c5703be25a47da
Bryce Simonds <bsimonds14@gmail.com>
2022-04-20 09:51:07 -0600

    Add 5 Array methods

--- arrays.rb
+++ arrays.rb
@@ -7,3 +7,18 @@ family_ages = [28, 2, 9, 3]
 family_cuteness_levels = [10.0, 9.9, 9.8, 9.7]
 
 family_payment_histories = [true, true, false, false]
+
+# Method will add Mom to end of array
+family_members.push ("Mom")
+
+# Method will return the first element (28) of the array
+family_ages.first
+
+# Method will return how many elements in the array starting at 1
+family_cuteness_levels.length
+
+# Method will remove the first array and return it
+family_payment_histories.shift
+
+# Method will add data to front of the array
+family_members.unshift ("Dad")
