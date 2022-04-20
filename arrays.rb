d1d1e21294b390048ef7d12f3115c43fda5bd9b5
Bryce Simonds <bsimonds14@gmail.com>
2022-04-20 09:42:41 -0600

    Add 4 different types of arrays

---
+++ arrays.rb
@@ -0,0 +1,9 @@
+family_members = ["Amanda", "Kora", "Riley"]
+
+family_members.push ("Plants")
+
+family_ages = [28, 2, 9, 3]
+
+family_cuteness_levels = [10.0, 9.9, 9.8, 9.7]
+
+family_payment_histories = [true, true, false, false]
